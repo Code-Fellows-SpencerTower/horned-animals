3060e1dd4336f800b8988dae46f68d9714790cfe
SpencerTower <spencerjtower1@gmail.com>
2021-11-29 17:20:29 -0800

    adds Header.js, Footer.js, removes files

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <p>Spencer Tower</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Horned Beasts</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
+
