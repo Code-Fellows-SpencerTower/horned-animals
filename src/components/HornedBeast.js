51aca542400d1f6a24423f6e1f10f598f857e1d4
SpencerTower <spencerjtower1@gmail.com>
2021-11-30 17:01:15 -0800

    adds state and handleClick() for favorite button

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,14 +1,28 @@
 import { Component } from 'react';
 
 export default class HornedBeast extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorites: 0
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ favorites: this.state.favorites + 1 });
+  }
+
+
   render() {
     return (
       <div>
         <h2>{this.props.title}</h2>
         <img src={this.props.imageURL} alt={this.props.title} title={this.props.title} />
         <p>{this.props.description}</p>
+        <p>❤️ {this.state.favorites}</p>
         {/* Heart emoji from: https://emojiguide.org/red-heart */}
-        <button onClick={this.handleClick}>❤️</button>
+        <button onClick={this.handleClick}>Favorite</button>
       </div>
     );
   }
