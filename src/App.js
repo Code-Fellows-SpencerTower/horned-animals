de8bc64572521c15f05d94f4d14dc9661e1c43e8
SpencerTower <spencerjtower1@gmail.com>
2021-12-01 16:57:05 -0800

    refactors to pass hornedBeasts data from App.js to Main.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Header from './components/Header.js';
 import Main from './components/Main.js';
 import Footer from './components/Footer.js';
+import hornedBeasts from './components/data.json';
 
 export default class App extends Component {
+
   render() {
     return (
       <div>
         <Header />
-        <Main />
+        <Main hornedBeasts={hornedBeasts} />
         <Footer />
       </div>
     );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,15 @@
 import { Component } from 'react';
 import { Row } from 'react-bootstrap';
 import HornedBeast from './HornedBeast';
-import hornedBeasts from './data.json';
+
 
 class Main extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     hornedBeasts:
-  //   };
 
   render() {
     return (
       <>
         <Row sm={1} md={2} lg={5}>
-          {hornedBeasts.map(beast => <HornedBeast beastAttribute={beast} />)}
+          {this.props.hornedBeasts.map(beast => <HornedBeast beastAttribute={beast} />)}
         </Row>
       </>
     );
