6767831727f4b80acb49e7cb4e7d986d58e32f6f
SpencerTower <spencerjtower1@gmail.com>
2021-12-02 14:57:55 -0800

    adds form to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from './components/Main.js';
 import Footer from './components/Footer.js';
 import beastData from './components/data.json';
 import SelectBeast from './components/SelectBeast.js';
+import Form from 'react-bootstrap/Form';
 
 export default class App extends Component {
 
@@ -30,6 +31,14 @@ export default class App extends Component {
     return (
       <div>
         <Header />
+        <Form>
+          <Form.Select onChange={this.handleChange}>
+            <option value="1-horn">1 horn</option>
+            <option value="2-horns">2 horns</option>
+            <option value="3-horns">3 horns</option>
+            <option value="3+-horns">3+ horns</option>
+          </Form.Select>
+        </Form>
         <Main beastData={beastData} showModal={this.showModal} closeModal={this.closeModal} />
         <SelectBeast show={this.state.showModal} beast={this.state.clickedBeast} closeModal={this.closeModal} />
         <Footer />
